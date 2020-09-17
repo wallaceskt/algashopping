f574cc2779f0e9e7ef256b1b83b8a62fc733f67a
Wallace Oliveira <wallacesales@gmail.com>
2020-09-17 15:35:49 -0300

    Instalei o styled-components.

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Wrapper } from './App.styles'
+
+function App () {
+    return <Wrapper>
+        todo: app
+    </Wrapper>
+}
+
+export default App
---
+++ src/components/App/App.styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const color = 'peachpuff';
+
+export const Wrapper = styled.div`
+    color: coral;
+    background-color: ${color};
+`
