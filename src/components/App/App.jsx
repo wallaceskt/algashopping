1045c54ab7ad7db7fe2dc8150a832338cb9c4654
Wallace Oliveira <wallacesales@gmail.com>
2020-09-17 23:05:04 -0300

    Adicionei o layout base.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import AppHeader from '../AppHeader/AppHeader'
+import AppContainer from '../AppContainer/AppContainer'
 import { Wrapper, Container } from './App.styles'
 
 function App () {
     return <Wrapper>
         <Container>
-            todo: app
+            <AppHeader />
+            <AppContainer
+                left={<div style={ { backgroundColor: 'red' } }>Produtos disponíveis</div>}
+                middle={<div style={ { backgroundColor: 'green' } }>Sua lista de compras</div>}
+                right={<div style={ { backgroundColor: 'blue' } }>Estatísticas</div>}
+            />
         </Container>
     </Wrapper>
 }
---
+++ src/components/AppHeader/AppHeader.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Wrapper } from './AppHeader.styles'
+
+function AppHeader() {
+    return <Wrapper>
+        <h1>alga <span>Shopping</span></h1>
+    </Wrapper>
+}
+
+export default AppHeader
